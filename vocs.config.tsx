d2fd1c8eb6d11bfb06f2fa5aecc6652c1e6de437
Ross Bulat <ross@jkrb.io>
2024-11-02 14:42:18 +0700

    add `withProviders

--- vocs.config.tsx
+++ vocs.config.tsx
@@ -41,6 +41,20 @@ export default defineConfig({
           link: '/library/extension-assets',
         },
         {
+          text: 'Factories',
+          collapsed: true,
+          items: [
+            {
+              text: "Overview",
+              link: '/library/factories/overview',
+            },
+            {
+              text: "withProviders",
+              link: '/library/factories/withProviders',
+            },
+          ]
+        },
+        {
           text: 'Hooks',
           collapsed: true,
           items: [
